fca0a8b976622a4de131b024ae5d9cca7503ea42
wang.xiaohu9 <wang.xiaohu9@ztesoft.com>
2018-11-11 20:29:13 +0800

    add lazy-loading

---
+++ Lazy-loading/README.md
@@ -0,0 +1,5 @@
+# react-training
+Lazy-loading components in React 16.6
+Code-splitting and lazy-loading React components with Suspense and React.lazy()
+
+https://blog.logrocket.com/lazy-loading-components-in-react-16-6-6cea535c0b52
---
+++ Lazy-loading/src/index.js
@@ -0,0 +1,41 @@
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import styled from "styled-components";
+import Loader from "./components/Loader";
+import ErrorBoundary from "./components/ErrorBoundary";
+
+import "./styles.css";
+
+const Calendar = React.lazy(() => {
+  return new Promise(resolve => setTimeout(resolve, 5 * 1000)).then(
+    () =>
+      Math.floor(Math.random() * 10) >= 4
+        ? import("./components/Calendar")
+        : Promise.reject(new Error())
+  );
+});
+
+const Header = styled.h1`
+  position: absolute;
+  width: 100%;
+  margin: 100px auto;
+`;
+
+function App() {
+  return (
+    <div className="App">
+      <ErrorBoundary>
+        <Header>Calendar</Header>
+
+        <Suspense maxDuration={300} fallback={<Loader />}>
+          <div className="calendar-container">
+            <Calendar />
+          </div>
+        </Suspense>
+      </ErrorBoundary>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
